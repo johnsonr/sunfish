17d2aa6ad0dabf880656f485028143760e464b69
Rod Johnson <johnsonroda@gmail.com>
2020-12-31 17:49:34 +1100

    Fix terminals

---
+++ evaluator.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Evaluator(ABC):
+
+    @abstractmethod
+    def score(pos, move):
+        pass
+
+    # @abstractmethod
+    # def terminals(self):
+    #     pass
