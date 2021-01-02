247c2c8778f231c50900656bc811fdcc2f4f15e4
Rod Johnson <johnsonroda@gmail.com>
2021-01-03 09:26:35 +1100

    General rules

--- evaluators.py
+++ evaluators.py
@@ -1,29 +1,35 @@
-from rule_based_eval import RuleBasedEvaluator
+from rule_based_eval import RuleBasedEvaluator, GameStage
 from pst_rules import createPstRules
-from queenrules import noEarlyQueenAdvance
 from playsharp_rules import developFast
 import kingrules
+import queenrules
 
 # ClassicalEvaluator
 # Tarrasch style
-ConservativeEvaluator = RuleBasedEvaluator([
-    createPstRules(piece={'P': 100, 'N': 300, 'B': 310,
-                          'R': 500, 'Q': 900, 'K': 60000}),
-    kingrules.mustBeAbleToCastle,
-    kingrules.noEarlyKingMoves,
-    kingrules.castleKingside(40),
-    kingrules.castleQueenside,
-    noEarlyQueenAdvance
-]
+ConservativeEvaluator = RuleBasedEvaluator(
+    generalRules=[
+        createPstRules(piece={'P': 100, 'N': 300, 'B': 320,
+                                        'R': 500, 'Q': 900, 'K': 60000})],
+    gameStageRules={
+        GameStage.Opening: (
+            kingrules.mustBeAbleToCastle,
+            kingrules.noEarlyKingMoves,
+            kingrules.castleKingside(40),
+            kingrules.castleQueenside,
+            queenrules.noEarlyQueenAdvance
+        )},
 )
 
-MorphyEvaluator = RuleBasedEvaluator([
-    createPstRules(piece={'P': 100, 'N': 300, 'B': 310,
-                          'R': 500, 'Q': 900, 'K': 60000}),
-    kingrules.mustBeAbleToCastle,
-    kingrules.noEarlyKingMoves,
-    kingrules.castleKingside(40),
-    kingrules.castleQueenside,
-    developFast(110),
-]
+MorphyEvaluator = RuleBasedEvaluator(
+    generalRules=[
+        createPstRules(piece={'P': 100, 'N': 300, 'B': 310,
+                                        'R': 500, 'Q': 900, 'K': 60000})],
+    gameStageRules={
+        GameStage.Opening: (
+            kingrules.mustBeAbleToCastle,
+            kingrules.noEarlyKingMoves,
+            kingrules.castleKingside(40),
+            kingrules.castleQueenside,
+            developFast(110),
+        )},
 )
